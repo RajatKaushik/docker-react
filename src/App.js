ec9553e331d5fbfd22c91d224aa221f8a435887b
Rajat Kaushik <m1046872@mindtree.com>
2019-06-22 13:34:45 +0530

    first commint on feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          On the feature branch!
         </a>
       </header>
     </div>
